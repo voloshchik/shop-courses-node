db2b894ff5c06be328ee5d330ea8ac481bc0586b
vladimir <lovkiy2012@gmail.com>
2020-03-20 18:03:12 +0300

    19 html

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
