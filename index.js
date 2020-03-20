ad916856834c5957600d71bc272b55b6740791e1
vladimir <lovkiy2012@gmail.com>
2020-03-20 19:39:00 +0300

    23 render start

--- index.js
+++ index.js
@@ -16,11 +16,21 @@ app.set("views", "views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Главная страница"
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get("/courses", (req, res) => {
+  res.render("courses", {
+    title: "Курсы"
+  });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Добавить курс"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
