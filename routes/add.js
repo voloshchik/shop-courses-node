42a254b3f91a051eca5acd10a8ed3e93836a4b3c
vladimir <lovkiy2012@gmail.com>
2020-03-21 20:58:42 +0300

    26 create modele

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-
+const Course = require("../modules/course");
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -9,8 +9,10 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/", async (req, res) => {
+  const course = new Course(req.body.title, req.body.price, req.body.img);
+  await course.save();
+
   res.redirect("/courses");
 });
 
