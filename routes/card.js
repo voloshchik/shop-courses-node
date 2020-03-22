8b6057892064635fcf6115716df50af851b26841
vladimir <lovkiy2012@gmail.com>
2020-03-22 19:01:19 +0300

    32 card finish

--- routes/card.js
+++ routes/card.js
@@ -1,22 +1,23 @@
+const { Router } = require("express");
+const Card = require("../models/card");
+const Course = require("../models/course");
 
-const {Router}=require('express')
-const Card=require('../models/card')
-const Course=require('../models/course')
+const router = Router();
 
-const router=Router()
+router.post("/add", async (req, res) => {
+  const course = await Course.getById(req.body.id);
+  await Card.add(course);
+  res.redirect("/card");
+});
 
-router.post('/add',async (req,res)=>{
-const course=Course.getById(req.body.id)
-await Card.addCard(course)
-res.redirect('/card')
-})
+router.get("/", async (req, res) => {
+  const card = await Card.fetch();
+  res.render("card", {
+    title: "Корзина",
+    isCard: true,
+    courses: card.courses,
+    price: card.price
+  });
+});
 
-router.get('/',async (req,res)=>{
-    const card= await Card.fetch()
-    res.render('card',{
-        title:"Корзина",
-        card
-    })
-})
-
-module.exports=router
+module.exports = router;
