2c1f3009ba440cb6d3ae118ef3a8798e5a24e3dc
vladimir <lovkiy2012@gmail.com>
2020-03-23 19:30:27 +0300

    34 delete card

--- models/card.js
+++ models/card.js
@@ -1,53 +1,73 @@
-const path = require('path')
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  'data',
-  'card.json'
-)
+  "data",
+  "card.json"
+);
 
 class Card {
   static async add(course) {
-    const card = await Card.fetch()
+    const card = await Card.fetch();
 
-    const idx = card.courses.findIndex(c => c.id === course.id)
-    const candidate = card.courses[idx]
+    const idx = card.courses.findIndex(c => c.id === course.id);
+    const candidate = card.courses[idx];
 
     if (candidate) {
       // курс уже есть
-      candidate.count++
-      card.courses[idx] = candidate
+      candidate.count++;
+      card.courses[idx] = candidate;
     } else {
       // нужно добавить курс
-      course.count = 1
-      card.courses.push(course)
+      course.count = 1;
+      card.courses.push(course);
     }
 
-    card.price += +course.price
+    card.price += +course.price;
 
     return new Promise((resolve, reject) => {
       fs.writeFile(p, JSON.stringify(card), err => {
         if (err) {
-          reject(err)
+          reject(err);
         } else {
-          resolve()
+          resolve();
         }
-      })
-    })
-  } 
+      });
+    });
+  }
+  static async remove(id) {
+    const card = await Card.fetch();
+    const idx = card.courses.findIndex(c => c.id === id);
+    const course = card.courses[idx];
+    if (course.count === 1) {
+      card.courses.filter(c => c.id !== id);
+    } else {
+      card.courses[idx].count--;
+    }
+    card.price -= course.price;
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(card);
+        }
+      });
+    });
+  }
 
   static async fetch() {
     return new Promise((resolve, reject) => {
-      fs.readFile(p, 'utf-8', (err, content) => {
+      fs.readFile(p, "utf-8", (err, content) => {
         if (err) {
-          reject(err)
+          reject(err);
         } else {
-          resolve(JSON.parse(content))
+          resolve(JSON.parse(content));
         }
-      })
-    })
+      });
+    });
   }
 }
 
-module.exports = Card
+module.exports = Card;
