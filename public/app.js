976317ed8d143e8b22a54857fb680495bf5d9ec7
vladimir <lovkiy2012@gmail.com>
2020-03-23 20:26:18 +0300

    35 dinamic params

--- public/app.js
+++ public/app.js
@@ -1,9 +1,14 @@
-document.querySelectorAll(".price").forEach(node => {
-  node.textContent = new Intl.NumberFormat("ru-RU", {
-    currency: "rub",
-    style: "currency"
-  }).format(node.textContent);
-});
+const toCurrency = price => {
+  return new Intl.NumberFormat('ru-RU', {
+    currency: 'rub',
+    style: 'currency'
+  }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+  node.textContent = toCurrency(node.textContent)
+})
+
 
 const $card = document.querySelector("#card");
 if ($card) {
@@ -15,8 +20,24 @@ if ($card) {
         method: "delete"
       })
         .then(res => res.json())
-        .then(data => {
-          console.log("data", data);
+        .then(card => {
+          if (card.courses.length) {
+            const html = card.courses.map(c => {
+              return `
+              <tr>
+                <td>${c.title}</td>
+                <td>${c.count}</td>
+                <td>
+                  <button class="btn btm-small js-remove" data-id="${c.id}">Удалить</button>
+                </td>
+              </tr>
+              `
+            }).join('')
+            $card.querySelector('tbody').innerHTML = html
+            $card.querySelector('.price').textContent = toCurrency(card.price)
+          } else {
+            $card.innerHTML = "<p>Курсов нет</p>";
+          }
         });
     }
   });
