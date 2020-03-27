23298ec511d44578b2492c79dd78a1dcf10111cc
vladimir <lovkiy2012@gmail.com>
2020-03-27 17:59:18 +0300

    44 render cart

--- routes/card.js
+++ routes/card.js
@@ -4,6 +4,25 @@ const Course = require("../models/course");
 
 const router = Router();
 
+function mapCartItems(cart) {
+  return cart.items.map(c => ({
+    ...c.courseId._doc,
+    count: c.count
+  }));
+}
+
+function computePrice(courses) {
+  return courses.reduce((total, course) => {
+    return total += course.price * course.count
+  }, 0)
+}
+
+// function computePrice(courses) {
+//   return courses.reducer((total, course) => {
+//     return (total += course.price * course.count);
+//   }, 0);
+// }
+
 router.post("/add", async (req, res) => {
   const course = await Course.findById(req.body.id);
   await req.user.addToCart(course);
@@ -15,15 +34,19 @@ router.delete("/remove:id", async (req, res) => {
   res.status(200).json(card);
 });
 
-router.get("/", async (req, res) => {
-  // const card = await Card.fetch()
-  // res.render('card', {
-  //   title: 'Корзина',
-  //   isCard: true,
-  //   courses: card.courses,
-  //   price: card.price
-  // })
-  res.json({ test: true });
-});
+router.get('/', async (req, res) => {
+  const user = await req.user
+    .populate('cart.items.courseId')
+    .execPopulate()
+
+  const courses = mapCartItems(user.cart)
+
+  res.render('card', {
+    title: 'Корзина',
+    isCard: true,
+    courses: courses,
+    price: computePrice(courses)
+  })
+})
 
 module.exports = router;
