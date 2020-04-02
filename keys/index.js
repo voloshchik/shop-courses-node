e33d80b88cab463565fcfdb2d51112d50182bca1
vladimir <lovkiy2012@gmail.com>
2020-04-02 22:07:56 +0300

    64 siting sengrid

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   MONGODB_URL:
     "mongodb+srv://vladimir:SRs1OrGV1zuNCLm6@cluster0-ikqau.mongodb.net/shop",
-  SESSION_SECRET: "Some secret value"
+  SESSION_SECRET: "Some secret value",
+  SENDGRID_API_KEY:'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'
 };
