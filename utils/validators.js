7ef64f23c6de2feca39fac16b1362b1a93feec4e
vladimir <lovkiy2012@gmail.com>
2020-04-07 18:50:02 +0300

    73

--- utils/validators.js
+++ utils/validators.js
@@ -7,24 +7,29 @@ exports.registerValidators = [
     .withMessage("Введите корректный email")
     .custom(async (value, { req }) => {
       try {
-        const user = await User.findOne({ email: value })
+        const user = await User.findOne({ email: value });
         if (user) {
           return Promise.reject("Такой email уже занят");
         }
       } catch (error) {
         console.log(error);
       }
-    }),
+    })
+    .normalizeEmail(),
   body("password", "Пороль должен быть миниум 6 символов")
     .isLength({ min: 6, max: 56 })
     .isAlphanumeric(),
-  body("confirm").custom((value, { req }) => {
-    if (value !== req.body.password) {
-      throw new Error("Пороли должны совпадать");
-    }
-    return true;
-  }),
+  trim(),
+  body("confirm")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Пороли должны совпадать");
+      }
+      return true;
+    })
+    .trim(),
   body("name")
     .isLength({ min: 3 })
-    .withMessage("Имя должно быть миниум три символа"),
+    .withMessage("Имя должно быть миниум три символа")
+    .trim(),
 ];
