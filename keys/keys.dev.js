f8ce1a73151bb94ea05bf939e67044b8f4240278
voloshchik <50056885+voloshchik@users.noreply.github.com>
2020-04-10 23:26:37 +0300

    delete keys

--- keys/keys.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://vladimir:SRs1OrGV1zuNCLm6@cluster0-ikqau.mongodb.net/shop',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'nodejs@email.ru',
-  BASE_URL: 'http://localhost:3000'
-}
