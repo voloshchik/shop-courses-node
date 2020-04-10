94a0edd30c3cee976777ccb2fd02c2cca1aec722
vladimir <lovkiy2012@gmail.com>
2020-04-10 18:41:54 +0300

    test2

--- index.js
+++ index.js
@@ -1,102 +1,85 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const path = require('path')
 const csrf = require('csurf')
-const flash=require('connect-flash')
-const mongoose = require("mongoose");
-const helmet=require('helmet')
-const compression=require('compression')
-const exphbs = require("express-handlebars");
-const session = require("express-session");
-const MongoStore=require('connect-mongodb-session')(session)
-const varMiddleware = require("./middleware/variables");
-const userMiddleware=require('./middleware/user')
-const fileMiddleware=require('./middleware/file')
-const keys=require('./keys')
+const flash = require('connect-flash')
+const mongoose = require('mongoose')
+const helmet = require('helmet')
+const compression = require('compression')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const authRoutes = require('./routes/auth')
+const profileRoutes = require('./routes/profile')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
+const errorHandler = require('./middleware/error')
+const fileMiddleware = require('./middleware/file')
+const keys = require('./keys')
 
-const homeRoutes = require("./routes/home");
-const cardRoutes = require("./routes/card");
-const addRoutes = require("./routes/add");
-const coursesRoutes = require("./routes/courses");
-const ordersRoutes = require("./routes/orders");
-const authRoutes = require("./routes/auth");
-const profileRoutes=require('./routes/profile')
+const PORT = process.env.PORT || 3000
 
-
-const errorHandler=require('./middleware/error')
-
-
-
-const app = express();
-
+const app = express()
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs',
   helpers: require('./utils/hbs-helpers')
 })
-
-
-
-
-
-const store=new MongoStore({
-  collection:'sessions',
-  uri:keys.MONGODB_URL
+const store = new MongoStore({
+  collection: 'sessions',
+  uri: keys.MONGODB_URI
 })
 
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "views");
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use('/images',express.static(path.join(__dirname,'images')))
-app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  session({
-    secret: keys.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store
-  })
-);
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+  secret: keys.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store
+}))
 app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
 app.use(helmet())
 app.use(compression())
-app.use(varMiddleware);
+app.use(varMiddleware)
 app.use(userMiddleware)
 
-app.use("/", homeRoutes);
-app.use("/add", addRoutes);
-app.use("/courses", coursesRoutes);
-app.use("/card", cardRoutes);
-app.use("/orders", ordersRoutes);
-app.use("/auth", authRoutes);
-app.use('/profile',profileRoutes)
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3000;
-
 async function start() {
   try {
-    
-    await mongoose.connect(keys.MONGODB_URL, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    });
-
-
+      useFindAndModify: false
+    })
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.log("error111", err);
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
   }
 }
 
-start();
+start()
+
 
